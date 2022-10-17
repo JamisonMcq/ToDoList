871b6cb6c0dbd9bcbd70c5f336d468adba606e25
Jamison Mcqueary <jamison.mcqueary@comcast.net>
2022-10-17 17:53:34 -0400

    Adding Buttons

--- frontend/todolist/src/app/additem/additem.component.ts
+++ frontend/todolist/src/app/additem/additem.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-additem',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./additem.component.css'],
 })
 export class AdditemComponent implements OnInit {
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit(): void {}
+
+  btnClick = () => {
+    //Need to add a new service to do post request to backend and call to service here
+    this.router.navigate(['']);
+  };
 }
